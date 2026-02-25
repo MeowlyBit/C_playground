61cff6e964a2a2f8ba0f70231fdcead06467b5bf
Weronika <262886253+MeowlyBit@users.noreply.github.com>
2026-02-25 21:12:54 +0100

    Printing values

--- test.c
+++ test.c
@@ -1,6 +1,23 @@
+//There are all of Module 1 tasks and informations
+
 #include <stdio.h>
-int main(int argc, char const *argv[])
-{
-    printf("Hello, Meow!\n");
-    return 0;
+int main(void){
+
+	int number = 1;
+
+	puts("meow"); //Printing something, but first i need include stdio.h file
+
+	printf("The value of five is: %d \n", 5); //It can print some value of decimal number (int) - using %d
+	printf("The value of six is: %d \n", 16 - 10);
+	printf("The value of seven is: %d \n", 7);
+	printf("The value of eight is: %d \n", (8000 - 7992));
+
+	//Hexadecimal (base 16) numbers
+	printf("The value of twenty-one is: %d \n", 0x15);
+	printf("The value of twenty-two is: %d \n", 0x16);
+	printf("The value of sixty-two is: %d \n", 0x3E);
+	printf("The value of seventy-four is: %d \n", 0x4A);
+	//But how does it works?
+
+	return 1; 
 }
