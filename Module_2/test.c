669ba1e29a37c7abecb6b6693feb67b06bfb6fff
Weronika <262886253+MeowlyBit@users.noreply.github.com>
2026-03-03 16:23:50 +0100

    Module 2, float numbers

---
+++ Module_2/test.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    //--- ex. 1
+	printf("The value of seven is: %f\n", 7.0);
+	printf("The value of eight and a half is: %f\n", 8.5);
+
+    //--- ex.2
+	float tenValue = 10.000000;
+	printf("The value of ten is: %f\n", tenValue);
+	printf("The value of nine is: %f\n", 9.0);
+
+    //--- ex.3
+	float halfValue = 0.5;
+	float piValue = 3.141593;
+	printf("The value of half is: %f\n", halfValue);
+	printf("The value of Pi is: %f\n", piValue);
+
+	return 0;
+}
